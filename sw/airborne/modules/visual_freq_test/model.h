813c9d360c86c4884be882d2c23ddf978ae10722
L-Simons <lievijn@outlook.com>
2025-03-11 17:47:05 +0100

    added xmls for frequency testing and added cnn module

---
+++ sw/airborne/modules/visual_freq_test/model.h
@@ -0,0 +1,7 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+// Function prototype
+extern void model_inference(const float tensor_args_0[1][240][520][3], float tensor_output[1][96]);
+
+#endif
---
+++ test.py
@@ -0,0 +1,3 @@
+import numpy 
+a= np.array([1,2,3])
+print(a)
